292b6f7808d03f2bac914058f0a3db98aa5b0c70
WeiYaoJia <weiyaojia@weiyaojiademacbook-pro.local>
2018-12-18 13:58:48 +0800

    Initial Commit

---
+++ Test9Second/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test9Second
+//
+//  Created by WeiYaoJia on 2018/12/18.
+//  Copyright © 2018年 WuChangyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
